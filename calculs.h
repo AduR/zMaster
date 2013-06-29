0846ba9c9d3c68b893dc14937b12fdd10b79979e
AduR <adrien.duranquet@laposte.net>
2013-06-29 18:54:10 +0200

    Create calculs.h

---
+++ calculs.h
@@ -0,0 +1,8 @@
+#ifndef CALCULS_H_INCLUDED
+#define CALCULS_H_INCLUDED
+
+    void setSolution(int (*solution)[TROU]);
+    void calculInfo(Master *master);
+    int nouveauRond(Master *master, int (*tabEcran)[LARGEUR_FENETRE/50][HAUTEUR_FENETRE/50]);
+
+#endif /* CALCULS_H_INCLUDED */
